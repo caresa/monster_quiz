c179ac87124f1adf2a0f6c81456371afa460d86e
Caresa Huddleston <caresa.huddleston@gmail.com>
2014-08-14 12:22:44 -0500

    revert back to way it was last nite

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,21 +16,8 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    quiz = Quiz.find(params[:quiz_id])
-    @quiz_question = quiz.questions.find(params[:id])
-
-    questions_array = quiz.questions.to_ary
-    question_index  = questions_array.index(@quiz_question)
-    @next_question  = questions_array[question_index + 1]
-    # render json: @quiz_question
-  end
-
-  def save_answer
-    if @next_question != nil
-      # redirect_to
-    else
-      # redirect_to results page
-    end
+    @quiz_question = Quiz.find(params[:quiz_id]).questions.find(params[:id])
+    render json: @quiz_question
   end
 
   def check_meta
